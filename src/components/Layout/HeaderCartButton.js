79d3483c9ef0455d9d24425b459cc06fe8aaed3f
Tojo Antonio ANDRIAMANGA <a.tojo.antonio@gmail.com>
2022-05-01 20:38:58 +0200

    143-create cart ctx and use it

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,15 +1,21 @@
-import React from "react";
+import React, { useContext } from "react";
 import classes from "./HeaderCartButton.module.css";
 import CartIcon from "../Cart/CartIcon";
+import CartContext from "../../store/cart-context";
 
 const HeaderCartButton = (props) => {
+  const cartCtx = useContext(CartContext);
+  const nbOfCartItems = cartCtx.items.reduce((curNb, item) => {
+    return curNb + item.amount;
+  }, 0);
+
   return (
     <button className={classes.button} onClick={props.onClick}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
       <span> Your cart</span>
-      <span className={classes.badge}>3</span>
+      <span className={classes.badge}>{nbOfCartItems}</span>
     </button>
   );
 };
---
+++ src/store/CartProvider.js
@@ -0,0 +1,21 @@
+import CartContext from "./cart-context";
+
+const CartProvider = (props) => {
+  const addItemToCartHandler = (item) => {};
+
+  const removeItemToCartHandler = (id) => {};
+
+  const cartCtx = {
+    items: [],
+    totalAmount: 0,
+    addItem: addItemToCartHandler,
+    removeItem: removeItemToCartHandler,
+  };
+  return (
+    <CartContext.Provider value={cartCtx}>
+      {props.children}
+    </CartContext.Provider>
+  );
+};
+
+export default CartProvider;
